a5dfa9c86067f56abd6b7a4fc808a55f48097977
Huy-No1 <71492647+Huy-No1@users.noreply.github.com>
2021-07-21 15:50:37 +0700

    3

--- src/App.js
+++ src/App.js
@@ -51,9 +51,9 @@ function App() {
         <motion.div className="pop"
               initial={{opacity: 0.7, y: -20}}
               animate={{opacity: 1, y: 0}}>
-          <p className="pop--header">He luu cậu!</p>
+          <p className="pop--header">He luu !</p>
           <img src="cat.jpg" alt="" className="pop--img"/>
-          <p className="pop--footer">Tớ có điều này muốn hỏi cậu nhớ phải trả lời thật lòng nhaaa.</p>
+          <p className="pop--footer">Anh có điều này muốn hỏi em nhớ phải trả lời thật lòng nhaaa.</p>
           <Link to="/choose" className="pop--button">
             <div >OK</div>
           </Link>
@@ -63,14 +63,14 @@ function App() {
         <motion.div className="choose"
               initial={{opacity: 0.7, y: -20}}
               animate={{opacity: 1, y: 0}}>
-          <p className="choose--header">Cậu thích tớ phải không nào ._.</p>
-          <p className="choose--header2">Nếu cậu không trả lời mà thoát ra thì phải làm vợ tớ đó nhaa</p>
+          <p className="choose--header">Em thích anh phải không ._.</p>
+          <p className="choose--header2">Nếu em không trả lời mà thoát ra thì phải làm vợ anh đó nhaa</p>
           <Link to="/note" className="choose--btn choose--btn__correct">
             <div>{'Yêu ơi là yêuuuuuu <3'}</div>
           </Link>
           <div className="choose--btn choose--btn__wrong" style={ps[positon]}
                onClick={handleWrong} >
-                Cậu mơ à ._.
+                Anh mơ à ._.
           </div>
         </motion.div>
       </Route>
@@ -78,12 +78,12 @@ function App() {
       <motion.div className="note"
           initial={{opacity: 0.7, y: -20}}
           animate={{opacity: 1, y: 0}}>
-        <p className="note--text">Thật không zậy. Nói cho tớ lí do vì sao cậu thích tớ đi</p>
+        <p className="note--text">Thật không zậy. Nói cho anh lí do vì sao em thích anh đi</p>
         <img src="cute.gif" className="note--img"/>
         <input value={text} onChange={handleNote} className="note--input"/>
         <Link to="/end" className="choose--btn note--btn__correct" onClick={ e => handleSend(e)}>
         <div>
-                Gửi cho tớ
+                Gửi cho anh
         </div>
         </Link>
       </motion.div>
@@ -92,7 +92,7 @@ function App() {
       <motion.div className="note"
           initial={{opacity: 0.7, y: -20}}
           animate={{opacity: 1, y: 0}}>
-        <p className="end--text">Ỏoooooo :>. Tớ biết mà!. Vậy cậu làm người yêu tớ nhé</p>
+        <p className="end--text">Ỏoooooo :>. Anh biết mà!. Vậy em làm người yêu anh nhé</p>
         <img src="end.gif" className="end--img"/>
       </motion.div>
       </Route>    
