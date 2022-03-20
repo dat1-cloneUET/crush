ddc97507f2fd6e3f7afd75e00568db7f4317675f
huytn <huytn@kaopiz.com>
2022-03-20 23:38:19 +0700

    abc

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
-import { Route, Link }  from "react-router-dom";
+import "./App.css";
+import { Route, Link } from "react-router-dom";
 import { motion } from "framer-motion";
-import { useState } from 'react';
+import { useState } from "react";
 
-const ps=[
-  {
-    // right: "10%",
-  },
+const ps = [
+  {},
   {
     left: "10%",
     top: "2%",
@@ -23,39 +20,37 @@ const ps=[
   {
     left: "5%",
     bottom: "0%",
-  }
-]
-const textt="Tại vì anh cute vl.... <3";
+  },
+];
+const textt = "Bởi vì thầy cô quá cute";
 function App() {
   const [positon, setPosition] = useState(0);
   const [text, setText] = useState("");
   const handleWrong = () => {
-    setPosition((positon + 1 ) % 5);
-  }
+    setPosition((positon + 1) % 5);
+  };
   const handleNote = () => {
-    if(text.length < textt.length) {
-      setText(textt.slice(0, text.length+1));
+    if (text.length < textt.length) {
+      setText(textt.slice(0, text.length + 1));
     }
-  }
+  };
   const handleSend = (e) => {
-    if(text.length < 16){
+    if (text.length < 16) {
       e.preventDefault();
       alert("Nhập chưa xong mà bấm gửi là sao -_-");
     }
-      
-  }
+  };
   return (
     <div className="main">
-      <div className="main--bg"/>
+      <div className="main--bg" />
       <Route path="/" exact>
         <motion.div className="pop"
               initial={{opacity: 0.7, y: -20}}
               animate={{opacity: 1, y: 0}}>
-          <p className="pop--header">He luu !</p>
-          <img src="cat2.jpg" alt="" className="pop--img"/>
-          <p className="pop--footer">Anh có điều này muốn hỏi em nhớ phải trả lời thật lòng nhaaa.</p>
+          <p className="pop--header">Hellu 10B15</p>
+          <img src="cat.jpg" alt="" className="pop--img"/>
           <Link to="/choose" className="pop--button">
-            <div >OK</div>
+            <div >Next</div>
           </Link>
         </motion.div>
       </Route>
@@ -63,14 +58,13 @@ function App() {
         <motion.div className="choose"
               initial={{opacity: 0.7, y: -20}}
               animate={{opacity: 1, y: 0}}>
-          <p className="choose--header">Em thích anh phải không ._.</p>
-          <p className="choose--header2">Nếu em không trả lời mà thoát ra thì phải làm vợ anh đó nhaa</p>
+          <p className="choose--header">10B15 có yêu thầy cô không??</p>
           <Link to="/note" className="choose--btn choose--btn__correct">
             <div>{'Yêu ơi là yêuuuuuu <3'}</div>
           </Link>
           <div className="choose--btn choose--btn__wrong" style={ps[positon]}
                onClick={handleWrong} >
-                Anh mơ à ._.
+                Khum
           </div>
         </motion.div>
       </Route>
@@ -78,24 +72,67 @@ function App() {
       <motion.div className="note"
           initial={{opacity: 0.7, y: -20}}
           animate={{opacity: 1, y: 0}}>
-        <p className="note--text">Thật không zậy. Nói cho anh lí do vì sao em thích anh đi</p>
+        <p className="note--text">Thật không zậy? Nói thầy cô biết lí do vì sao thích đi </p>
         <img src="cute.gif" className="note--img"/>
         <input value={text} onChange={handleNote} className="note--input"/>
-        <Link to="/end" className="choose--btn note--btn__correct" onClick={ e => handleSend(e)}>
+        <Link to="/1" className="choose--btn note--btn__correct" onClick={ e => handleSend(e)}>
         <div>
-                Gửi cho anh
+                Gửi
         </div>
         </Link>
       </motion.div>
       </Route>  
-      <Route path="/end">
-      <motion.div className="note"
-          initial={{opacity: 0.7, y: -20}}
-          animate={{opacity: 1, y: 0}}>
-        <p className="end--text">Ỏoooooo :>. Anh biết mà!. Vậy em làm người yêu anh nhé</p>
-        <img src="end.gif" className="end--img"/>
-      </motion.div>
-      </Route>    
+
+      <Route path="/1" exact>
+        <motion.div
+          className="pop"
+          initial={{ opacity: 0.7, y: -20 }}
+          animate={{ opacity: 1, y: 0 }}
+        >
+          <img src="pic1.jpg" alt="" className="pop--img" />
+          <p className="pop--footer">1. Tự tin xin điểm 10!</p>
+          <p className="pop--footer">2. Bình tĩnh - tự tin - chiến thắng</p>
+          <p className="pop--footer">
+            3. Chúc tất cả các thành viên của 10B15 “không điểm trên tay, mười
+            điểm trên giấy”, “cá chép hóa rồng” và “vượt cạn thành công” nhé!{" "}
+          </p>
+          <Link to="/2" className="pop--button">
+            <div>Next</div>
+          </Link>
+        </motion.div>
+      </Route>
+      <Route path="/2" exact>
+        <motion.div
+          className="pop"
+          initial={{ opacity: 0.7, y: -20 }}
+          animate={{ opacity: 1, y: 0 }}
+        >
+          <img src="pic2.jpg" alt="" className="pop--img" />
+          <p className="pop--footer">4. 10B15 - Try your best!!! </p>
+          <p className="pop--footer">5. Cẩn tắc vô áy náy</p>
+          <p className="pop--footer">
+            6. Ngày mai lên thớt rồi, đừng suy nghĩ gì nữa. Hãy ăn thật no, ngủ
+            thật kỹ để lấy sức chiến đấu nào. Chúng ta sẽ cùng làm thật tốt
+            trong kỳ thi này nhé!
+          </p>
+          <Link to="/3" className="pop--button">
+            <div>Next</div>
+          </Link>
+        </motion.div>
+      </Route>
+      <Route path="/3" exact>
+        <motion.div
+          className="pop"
+          initial={{ opacity: 0.7, y: -20 }}
+          animate={{ opacity: 1, y: 0 }}
+        >
+          <img src="pic3.jpg" alt="" className="pop--img" />
+          <p className="pop--footer">
+            7. Các bạn hãy chiến thắng chính bản thân mình- Áp lực tạo kim cương
+            nha
+          </p>
+        </motion.div>
+      </Route>
     </div>
   );
 }
